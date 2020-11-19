e5cd48c2472fbba91a494dc6687dba86bcd864de
dparksy <dylanpark.net@gmail.com>
2020-11-19 16:39:09 +1000

    Initial commit

---
+++ app/src/main/java/com/kidsdrawingapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kidsdrawingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawing_view.setSizeForBrush(20.toFloat()) // Setting the default brush size to drawing view.
+    }
+}
