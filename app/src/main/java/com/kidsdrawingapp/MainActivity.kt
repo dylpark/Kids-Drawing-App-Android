8f14611b8648a82d80249c858f0a0ebdd473a2e2
dparksy <dylanpark.net@gmail.com>
2020-11-24 19:02:01 +1000

    Update MainActivity.kt

--- app/src/main/java/com/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/kidsdrawingapp/MainActivity.kt
@@ -1,15 +1,57 @@
 package com.kidsdrawingapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawing_view.setSizeForBrush(20.toFloat()) // Setting the default brush size to drawing view.
+
+        mImageButtonCurrentPaint = ll_paint_colors [1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.palette_pressed)
+        )
+
+        ib_brush.setOnClickListener {
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size: ")
+
+        val smallBtn = brushDialog.ib_small_brush
+        smallBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumBtn = brushDialog.ib_medium_brush
+        mediumBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeBtn = brushDialog.ib_large_brush
+        largeBtn.setOnClickListener {
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
